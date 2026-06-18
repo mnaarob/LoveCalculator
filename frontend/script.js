4fb6012af8b3b02170008acf1d5c4c3b623b5b7a
MILLADUNNABE AROB <mn_aarob@MAC.local>
2026-06-17 21:27:34 -0500

    update website

--- frontend/script.js
+++
@@ -1,7 +0,0 @@
-function calculateLove() {
-  var name1 = document.getElementById("yourName").value;
-  var name2 = document.getElementById("yourPartnersName").value;
-  var score = Math.floor((Math.random()*100)+1);
-
-  document.getElementById("result").textContent = name1+" ❤️ "+name2+" = "+score+"%";
-}
